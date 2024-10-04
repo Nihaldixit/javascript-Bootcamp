c3e607c08ee34d365627ce86f1daa486f37a935b
NihalDixit <nhldxt026@gmail.com>
2024-10-04 17:56:52 +0530

    solution to looping a trianle exercise

---
+++ loopingATriangle.js
@@ -0,0 +1,15 @@
+/* this code prints the pattern
+#
+##
+###
+####
+#####
+######
+####### */
+
+let pattern = "";
+for (let i = 1; i < 8; i++) {
+  pattern += "#";
+  console.log(pattern);
+}
+//use command 'node loopingATriangle.js' to run this code'
